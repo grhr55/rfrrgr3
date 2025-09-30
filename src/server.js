0e767275fc0b90fbd2c3adea0e337053a0ef42e4
grhr55 <137615112+grhr55@users.noreply.github.com>
2025-09-30 22:34:38 +0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const childProcess = require('child_process');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/download') {
+    const file = 'hello.sh';
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('Ошибка чтения файла');
+      } else {
+        res.setHeader('Content-Disposition', `attachment; filename="${file}"`);
+        res.setHeader('Content-Type', 'application/x-sh');
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/run') {
+    const file = 'hello.sh';
+    childProcess.exec(`chmod +x ${file} && ./${file}`, (err, stdout, stderr) => {
+     
+    });
+   
+  } else {
+   
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Сервер запущен на порту 3000');
+});
