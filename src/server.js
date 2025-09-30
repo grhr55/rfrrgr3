386c4a67c346ed6a5f1eac54eeaa2d30b5c0b802
grhr55 <137615112+grhr55@users.noreply.github.com>
2025-10-01 00:05:37 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 const childProcess = require('child_process');
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 7000;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/download') {
-    const file = 'src/hello.sh';
+    const file = 'hello.sh';
     fs.readFile(file, (err, data) => {
       if (err) {
         console.error(err);
@@ -14,7 +14,7 @@ const server = http.createServer((req, res) => {
       } else {
         res.setHeader('Content-Disposition', `attachment; filename="${file}"`);
         res.setHeader('Content-Type', 'application/x-sh');
-        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.end(data);
@@ -23,15 +23,19 @@ const server = http.createServer((req, res) => {
   } else if (req.url === '/run') {
     const file = './hello.sh';
     childProcess.exec(`chmod +x ${file} && ./${file}`, (err, stdout, stderr) => {
-     
+      if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
     });
-   
   } else {
-   
+    res.statusCode = 404;
+    res.end('Не найдено');
   }
 });
 
 server.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
-
