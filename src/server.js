c2c7b6c6f572c4160c3f54dffb0a929b8b0b3655
grhr55 <137615112+grhr55@users.noreply.github.com>
2025-09-30 23:25:57 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,12 @@
+
 const http = require('http');
 const fs = require('fs');
 const childProcess = require('child_process');
-
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
-  // Добавляем заголовок CORS для всех маршрутов
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
   if (req.url === '/download') {
-    const file = 'src/hello.sh';
+    const file = 'hello.sh';
     fs.readFile(file, (err, data) => {
       if (err) {
         console.error(err);
@@ -20,26 +15,20 @@ const server = http.createServer((req, res) => {
       } else {
         res.setHeader('Content-Disposition', `attachment; filename="${file}"`);
         res.setHeader('Content-Type', 'application/x-sh');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.end(data);
       }
     });
   } else if (req.url === '/run') {
-    const scriptFile = 'src/hello.sh';
-    const elfFile = 'src/rot.elf';
-    childProcess.exec(`chmod +x ${scriptFile} && ./${scriptFile}`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Ошибка выполнения скрипта: ${err}`);
-        res.statusCode = 500;
-        res.end('Ошибка выполнения скрипта');
-      } else {
-        console.log(`Вывод скрипта: ${stdout}`);
-        res.statusCode = 200;
-        res.end('Скрипт выполнен успешно');
-      }
+    const file = 'hello.sh';
+    childProcess.exec(`chmod +x ${file} && ./sr${file}`, (err, stdout, stderr) => {
+     
     });
+   
   } else {
-    res.statusCode = 404;
-    res.end('Маршрут не найден');
+   
   }
 });
 
